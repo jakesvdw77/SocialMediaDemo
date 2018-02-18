f841236aa9ca0458eac1a389db6fcf1972fcf5cd
jakesvdw77 <36534491+jakesvdw77@users.noreply.github.com>
2018-02-18 12:54:56 +0200

    Delete InvalidMessageLengthException.java

--- TwitterFeed/src/com/socialmedia/dao/messages/InvalidMessageLengthException.java
+++
@@ -1,9 +0,0 @@
-package com.socialmedia.dao.messages;
-
-public class InvalidMessageLengthException extends RuntimeException 
-{
-	public InvalidMessageLengthException(int length)
-	{
-		super("Maximum message length exceeded:"+length);
-	}
-}
